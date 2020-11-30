813360f428e6a9a33dc043928ac9cd19a176c063
Lucas Andrade <lwerneck1994@gmail.com>
2020-11-30 12:40:57 -0300

    First Javascript file

---
+++ JS/workshop.js
@@ -0,0 +1,13 @@
+function buttonFunctionOnMouseOver()
+{
+    let button = document.getElementsByTagName("button")[0];
+    button.style.color= "red";
+}
+
+function buttonFunctionOnMouseOut()
+{
+    let button1 = document.getElementsByTagName("button")[0];
+    button1.style.color = "white"
+}
+
+
